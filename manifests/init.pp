f47363c2f02bc23b8dac6000441a3d814fcae243
Toby Griffiths <toby@cubicmushroom.co.uk>
2014-12-07 10:42:55 +0000

    Added Default Folder v4.6.11 install from package

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,7 @@
 # This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+class defaultfolder {
+  package { 'defaultfolder':
+    provider => 'pkgdmg',
+    source   => 'http://stclairsoft.s3.amazonaws.com/DefaultFolderX-4.6.11.dmg'
+  }
 }
