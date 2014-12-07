6c6f0d8683e3ea61f408679e6fcc1adb9f39abb4
Toby Griffiths <toby@cubicmushroom.co.uk>
2014-12-07 23:39:05 +0000

    Tweaking install as not working

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,9 @@
 # This is a placeholder class.
 class defaultfolder {
-  package { 'defaultfolder':
+  package { 'DefaultFolderX-4.6.11.dmg':
+    ensure   => 'installed',
     provider => 'pkgdmg',
-    source   => 'http://stclairsoft.s3.amazonaws.com/DefaultFolderX-4.6.11.dmg'
+    source   => 'http://stclairsoft.s3.amazonaws.com/DefaultFolderX-4.6.11.dmg',
+    alias    => 'defaultfolder'
   }
 }
